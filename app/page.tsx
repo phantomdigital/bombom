901d8b7b4dea91e816b06ebac2496147facf4fa0
phantomdigital <tom@phantomdigital.au>
2025-10-02 11:03:54 +1000

    Padding changes to hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PerspectiveGallery from '@/components/perspective-gallery';
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-5rem)] sm:h-[calc(100vh-6rem)] [@media(min-width:768px)_and_(max-width:1023px)]:h-[45vh] lg:h-[calc(100vh-11rem)] px-4 lg:px-24 xl:px-32 2xl:px-40 [@media(min-width:1795px)]:px-48 py-2 sm:py-3 lg:py-6">
+    <div className="h-[calc(100vh-5rem)] sm:h-[calc(100vh-6rem)] [@media(min-width:768px)_and_(max-width:1023px)]:h-[45vh] lg:h-[calc(100vh-11rem)] px-4 lg:px-10 xl:px-16 2xl:px-18 [@media(min-width:1795px)]:px-48 py-2 sm:py-3 lg:py-6">
       <div className="w-full h-full overflow-hidden rounded-2xl sm:rounded-3xl lg:rounded-4xl bg-[#ed5878] flex items-center justify-center">
         <PerspectiveGallery />
       </div>
